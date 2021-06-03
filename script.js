3672fe4521022e348ea7f611d0cbf7575e2f1581
GianluigiCelentano <cgianluigi96@gmail.com>
2021-06-03 15:20:59 +0200

    js-lista-cognomi

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var output = document.getElementById("surnames")
 var surname = prompt("inserisci il tuo cognome")
 var surnameList = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"]
 surnameList.push(surname)
-for (i = 6; i <= surnameList.length; i++) {
-    output.innerHTML += surnameList.sort()
+for (i = 1; i <= surnameList.length; i++) {
+    output.innerHTML = surnameList.sort()
 }
